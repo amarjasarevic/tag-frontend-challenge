cca1b3e694a8178c905890369db96dd66023979b
Amar Jasarevic <amarjasarevic@hotmail.com>
2022-04-14 14:13:11 +0200

    Fix eslint errors and warnings

--- puzzle/script.js
+++ puzzle/script.js
@@ -1,21 +1,21 @@
-(function() {
+(() => {
   const LEVELS = { EASY: 3, HARD: 4 };
 
   const DATA_X = 'data-position-x';
   const DATA_Y = 'data-position-y';
 
-  function hasElementAtPosition(x, y) {
+  const hasElementAtPosition = (x, y) => {
     const nodes = document.querySelectorAll(`[${DATA_X}="${x}"][${DATA_Y}="${y}"]`);
     return nodes.length > 0;
-  }
+  };
 
-  function getPositionFromElement(element) {
+  const getPositionFromElement = (element) => {
     const x = Number(element.getAttribute(DATA_X));
     const y = Number(element.getAttribute(DATA_Y));
     return { x, y };
-  }
+  };
 
-  function updateElementsPosition(element, level) {
+  const updateElementsPosition = (element, level) => {
     const { x, y } = getPositionFromElement(element);
 
     if (y !== 0 && !hasElementAtPosition(x, y - 1)) {
@@ -31,32 +31,32 @@
       // if not last column and there is no element on right => move right
       element.setAttribute(DATA_X, String(x + 1));
     }
-  }
+  };
 
-  function shuffleArray(array) {
+  const shuffleArray = (array) => {
     let currentIndex = array.length;
     let randomIndex;
 
     while (currentIndex !== 0) {
       randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
+      currentIndex -= 1;
 
       [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
     }
-  }
+  };
 
-  function getRandomArray(maxElements) {
+  const getRandomArray = (maxElements) => {
     const array = [];
     for (let i = 0; i <= maxElements; i += 1) { array.push(i); }
 
     shuffleArray(array);
 
     return array;
-  }
+  };
 
-  function generateInitialArray(level) {
+  const generateInitialArray = (level) => {
     const tiles = [];
-    const maxElements = Math.pow(2, level);
+    const maxElements = 2 ** level;
     const numbersArray = getRandomArray(maxElements);
 
     let index = 0;
@@ -72,16 +72,16 @@
     }
 
     return tiles;
-  }
+  };
 
-  function moveElement(element) {
+  const moveElement = (element) => {
     const { x, y } = getPositionFromElement(element);
     const size = element.offsetHeight;
 
     element.style.transform = `translateX(${x * size}px) translateY(${y * size}px)`;
-  }
+  };
 
-  function updateUI(tiles) {
+  const updateUI = (tiles) => {
     const elements = document.getElementsByTagName('li');
     for (let i = 0; i < elements.length; i += 1) {
       elements[i].innerHTML = tiles[i].number;
@@ -90,18 +90,16 @@
 
       moveElement(elements[i]);
     }
-  }
+  };
 
-  function handleListClick(level) {
-    return function (event) {
-      if (event.target.tagName !== 'LI') return;
+  const handleListClick = (level) => (event) => {
+    if (event.target.tagName !== 'LI') return;
 
-      updateElementsPosition(event.target, level);
-      moveElement(event.target);
-    }
-  }
+    updateElementsPosition(event.target, level);
+    moveElement(event.target);
+  };
 
-  function startNewGame(level) {
+  const startNewGame = (level) => {
     const tiles = generateInitialArray(level);
     updateUI(tiles);
 
@@ -110,7 +108,7 @@
 
     listElements[0].classList.add(`level-${level}`);
     listElements[0].addEventListener('click', handleListClick(level));
-  }
+  };
 
   startNewGame(LEVELS.EASY);
 
